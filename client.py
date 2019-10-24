d269469f0351372d8b54d978d80e9d99e34154c9
Isaac Merchan Blanco <imerchan@f-l3207-pc44.aulas.etsit.urjc.es>
2019-10-24 17:05:33 +0200

    server y puerto por la entrada

--- client.py
+++ client.py
@@ -4,15 +4,20 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
-
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+import sys
+# Constantes. Dirección IP del servidor,Puerto del servidor y
+# Linea
+try:
+    IPSERVER = sys.argv[1]
+    PORTSERVER = int(sys.argv[2])
+    # Une las palabras con lo que hay entre ' ',
+    LINE = ' '.join(sys.argv[3:])
+except IndexError:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((IPSERVER, PORTSERVER))
     print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -21,9 +21,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', 6001), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
